0dd3389d51c9fb2ee403727db5985752e6a400b0
Shirly Kadosh <shirlyk@gett.com>
2018-11-28 14:56:01 +0200

    Add new android project

---
+++ AndroidArchitecture/app/src/main/java/cookie/monster/developer/androidarchitecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package cookie.monster.developer.androidarchitecture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
